7cbbd29f3789e455ddfa6f30df26a8b685b9ba6e
Mattias Persson <mattias@panurgic.se>
2019-03-12 11:40:19 +0100

    Added a simple support for section

--- Sources/iOS/IndexPathConverter.swift
+++ Sources/iOS/IndexPathConverter.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public struct ChangeWithIndexPath {
-  
+
   public let inserts: [IndexPath]
   public let deletes: [IndexPath]
   public let replaces: [IndexPath]
@@ -29,9 +29,9 @@ public struct ChangeWithIndexPath {
 }
 
 public class IndexPathConverter {
-  
+
   public init() {}
-  
+
   public func convert<T>(changes: [Change<T>], section: Int) -> ChangeWithIndexPath {
     let inserts = changes.compactMap({ $0.insert }).map({ $0.index.toIndexPath(section: section) })
     let deletes = changes.compactMap({ $0.delete }).map({ $0.index.toIndexPath(section: section) })
@@ -42,7 +42,7 @@ public class IndexPathConverter {
         to: $0.toIndex.toIndexPath(section: section)
       )
     })
-    
+
     return ChangeWithIndexPath(
       inserts: inserts,
       deletes: deletes,
@@ -53,8 +53,47 @@ public class IndexPathConverter {
 }
 
 extension Int {
-  
+
   fileprivate func toIndexPath(section: Int) -> IndexPath {
     return IndexPath(item: self, section: section)
+  }
+}
+
+
+public struct ChangeWithSectionIndex {
+  public let inserts: [Int]
+  public let deletes: [Int]
+  public let replaces: [Int]
+  public let moves: [(from: Int, to: Int)]
+
+  public init(
+    inserts: [Int],
+    deletes: [Int],
+    replaces:[Int],
+    moves: [(from: Int, to: Int)]) {
+
+    self.inserts = inserts
+    self.deletes = deletes
+    self.replaces = replaces
+    self.moves = moves
+  }
+
+  public static func convert<T>(changes: [Change<T>]) -> ChangeWithSectionIndex {
+    let inserts = changes.compactMap({ $0.insert }).map({ $0.index })
+    let deletes = changes.compactMap({ $0.delete }).map({ $0.index })
+    let replaces = changes.compactMap({ $0.replace }).map({ $0.index })
+    let moves = changes.compactMap({ $0.move }).map({
+      (
+        from: $0.fromIndex,
+        to: $0.toIndex
+      )
+    })
+
+    return ChangeWithSectionIndex(
+      inserts: inserts,
+      deletes: deletes,
+      replaces: replaces,
+      moves: moves
+    )
   }
 }
--- Sources/iOS/UICollectionView+Extensions.swift
+++ Sources/iOS/UICollectionView+Extensions.swift
@@ -61,3 +61,50 @@ public extension UICollectionView {
   }
 }
 
+public extension UICollectionView {
+
+  /// Animate reload in a batch update
+  ///
+  /// - Parameters:
+  ///   - changes: The changes from diff
+  ///   - updateData: Update your data source model
+  ///   - completion: Called when operation completes
+  public func reloadSections<T: DiffAware>(
+    changes: [Change<T>],
+    updateData: () -> Void,
+    completion: ((Bool) -> Void)? = nil) {
+
+    let changesWithSectionIndex = ChangeWithSectionIndex.convert(changes: changes)
+
+    performBatchUpdates({
+      sectionReloads(changesWithSectionIndex: changesWithSectionIndex)
+      updateData()
+      sectionRearranges(changesWithSectionIndex: changesWithSectionIndex)
+    }, completion: { finished in
+      completion?(finished)
+    })
+  }
+
+  private func sectionRearranges(changesWithSectionIndex: ChangeWithSectionIndex) {
+    changesWithSectionIndex.deletes.executeIfPresent {
+      deleteSections(IndexSet($0))
+    }
+
+    changesWithSectionIndex.inserts.executeIfPresent {
+      insertSections(IndexSet($0))
+    }
+
+    changesWithSectionIndex.moves.executeIfPresent {
+      $0.forEach { move in
+        moveSection(move.from, toSection: move.to)
+      }
+    }
+  }
+
+  private func sectionReloads(changesWithSectionIndex: ChangeWithSectionIndex) {
+    changesWithSectionIndex.replaces.executeIfPresent {
+      self.reloadSections(IndexSet($0))
+    }
+  }
+}
+
